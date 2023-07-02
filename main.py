31f718501911e21ae6601984e72d5370a6572028
Insaneoftelegram <104971406+Insaneoftelegram@users.noreply.github.com>
2023-07-02 20:19:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import requests
 import youtube_dl
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-API_ID = "5642193"
-API_HASH = "c28fc9ac88530587236175da89184d75"
-BOT_TOKEN = "5208962076:AAFRivp9NqIABXvc0m8FI7PFxtaViYQeyeM"
 
 def download_song(song_url):
   """Downloads a song from YouTube and saves it to the current directory."""
   response = requests.get(song_url)
   song_info = youtube_dl.extract_info(response.content, download=False)
-  song_filename = f"{song_info['track']}.mp3"
+  song_filename = f"{song_info['title']}.mp3"
+
+  # Check if the song already exists
+  if os.path.exists(song_filename):
+    print(f"The song '{song_filename}' already exists.")
+    return
+
   with open(song_filename, "wb") as f:
     f.write(response.content)
 
+  print(f"Successfully downloaded the song '{song_filename}'.")
+
+def handle_message(update, context):
+  """Handles a message from a user."""
+  message = update.message.text
+
+  if message.startswith("/download"):
+    song_url = message.split(" ")[1]
+    download_song(song_url)
+
 def main():
-  song_url = input("Enter the URL of the song you want to download: ")
-  download_song(song_url)
+  """Starts the bot."""
+  bot = TelegramBot(token="5208962076:AAFRivp9NqIABXvc0m8FI7PFxtaViYQeyeM")
+  bot.register_handler(handle_message)
+  bot.run()
 
 if __name__ == "__main__":
   main()
